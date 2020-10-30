25f1cfa00da6f6d91e8d3dc6fcec2cf4086f4b5e
Ferdi Redelinghuys <ferdi.redelinghuys@vulamobile.com>
2020-10-30 10:11:20 +0200

    seperate custom widgets

---
+++ lib/icon_content.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const labelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+class IconContent extends StatelessWidget {
+  final IconData iconData;
+  final String label;
+
+  IconContent({@required this.label, @required this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: labelTextStyle.fontSize,
+            color: labelTextStyle.color,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_cart.dart';
 
 const bottomContainerHeight = 80.0;
 const buttonColour = Color(0xFF1D1E33);
@@ -26,7 +28,7 @@ class _InputPageState extends State<InputPage> {
                   child: ReusableCard(
                     colour: buttonColour,
                     cardChild: IconContent(
-                      title: 'MALE',
+                      label: 'MALE',
                       iconData: FontAwesomeIcons.mars,
                     ),
                   ),
@@ -35,7 +37,7 @@ class _InputPageState extends State<InputPage> {
                   child: ReusableCard(
                     colour: buttonColour,
                     cardChild: IconContent(
-                      title: 'FEMALE',
+                      label: 'FEMALE',
                       iconData: FontAwesomeIcons.venus,
                     ),
                   ),
@@ -73,56 +75,6 @@ class _InputPageState extends State<InputPage> {
             height: bottomContainerHeight,
           )
         ],
-      ),
-    );
-  }
-}
-
-class IconContent extends StatelessWidget {
-  final Color _colour = Color(0xFF8D8E98);
-  final IconData iconData;
-  final String title;
-
-  IconContent({@required this.title, @required this.iconData});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          iconData,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: _colour,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color colour;
-  final Widget cardChild;
-
-  ReusableCard({@required this.colour, this.cardChild});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
